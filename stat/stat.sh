568c72a88f95fab5887add9f3827b92273c25f38
Aleksandr Panfilov <aaa2ppp@gmail.com>
2024-11-14 14:44:43 +0300

    stat/202411141419

--- stat/stat.sh
+++ stat/stat.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-pref=202411
+pref='20241[01]*'
 prev=
 
-for d in $(ls -d ${pref}*); do
+for d in $(ls -d ${pref} | sort); do
     for i in 1 2 3; do
         if ! [ -f $d/less$i.csv ] && [ -f $d/less$i.html ]; then
             ./bin/parse $d/less$i.html > $d/less$i.csv
         fi
+
+        if ! [ -f $d/asterisk$i ] && [ -f $prev/asterisk$i ]; then
+            cp $prev/asterisk$i $d/asterisk$i
+        fi 
 
         if ! [ -f $d/less$i.csv ] && [ -f $prev/less$i.csv ]; then
             cp $prev/less$i.csv $d/less$i.csv
